2fea872003f6663dd3187ac7d83904d815166679
Admin <a.kalinin.de@gmail.com>
2024-02-07 15:21:51 +0100

    PROOOOOOGRAM

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,18 @@
-package org.example
-
 fun main() {
-    println("Hello World!")
+    val rows = 10
+    val cols = 10
+    val matrix = Array(rows) { Array(cols) {0} }
+
+    for (i in 0 until rows) {
+        for (j in 0 until cols) {
+            matrix[i][j] = (j * cols) + i + 1
+        }
+    }
+
+    for (i in 0 until rows) {
+        for (j in 0 until cols) {
+            print("${matrix[i][j]}\t")
+        }
+        println()
+    }
 }
