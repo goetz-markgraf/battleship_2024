f0ca4ee66b95cab0c3cf34afdea7a09efabe1e6d
Admin <a.kalinin.de@gmail.com>
2024-02-07 15:49:46 +0100

    PROOOOOOGRAM

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,28 @@
 fun main() {
     val rows = 10
     val cols = 10
-    val matrix = Array(rows) { Array(cols) {0} }
+    val matrix = Array(rows) { Array(cols) { " " } }
+
 
     for (i in 0 until rows) {
         for (j in 0 until cols) {
-            matrix[i][j] = (j * cols) + i + 1
+            matrix[i][j] = " "
         }
     }
 
-    for (i in 0 until rows) {
-        for (j in 0 until cols) {
-            print("${matrix[i][j]}\t")
+
+    print("   ")
+    for (i in 1..cols) {
+        print("$i ")
+    }
+    println()
+    val letters= ('A'..'J').toList()
+    for(i in 0 until rows){
+        print("${letters[i]}|")
+        for(j in 0 until cols){
+            print("${matrix[i][j]}|")
         }
         println()
     }
+
 }
