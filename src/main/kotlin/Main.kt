628feddace1b67558aa1b68e880de0d25f1c30e1
Arsen <arsenadamen@gmail.com>
2024-02-08 01:52:29 +0100

    inputIsValid()

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,9 @@
+import kotlin.math.abs
+
+enum class Function {
+    CHECK, PLACE
+}
+
 fun main() {
 
     val fields = createFields()
@@ -28,25 +34,61 @@ fun createFields(): Pair<Array<Array<Int>>, Array<Array<Int>>> {
 
 fun showField(field: Array<Array<Int>>) {
 
-    print("\t  ")
     for (i in '0'..'9') {
-        print("$i   ")
+        print("\t  $i")
     }
     println()
 
+    val tabVertLine = "\t${9474.toChar()}"
+
     for (i in 0 ..< 10) {
-        print(" ${(i + 65).toChar()}  |")
+        print(" " + (i + 65).toChar() + tabVertLine)
         for (j in 0 ..< 10) {
-            print(" ${69.toChar()} |")
+            print(tabVertLine)
         }
         println()
     }
 }
 
 fun place(field: Array<Array<Int>>): Array<Array<Int>> {
+    val location = readln().lowercase()
+    if (inputIsValid(Function.PLACE, location)) {
+
+    }
     return field
 }
 
-fun check(): Boolean? {
+fun check() {
+    val pos = readln().lowercase()
+    if (inputIsValid(Function.CHECK, pos)) {
+
+    }
+}
+
+fun inputIsValid(function: Function, input: String): Boolean {
+    when (function) {
+        Function.CHECK ->
+            if (input[0] in 'a'..'j' && input[1] in '0'..'9') {
+                return true
+            } else {
+                println("Enter the cell index in format \"letternumber\", for example \"a0\"")
+                return false
+            }
+
+        Function.PLACE ->
+            if (input[0] in 'a'..'j' && input[1] in '0'..'9' &&
+                input[2] in 'a'..'j' && input[3] in '0'..'9' &&
+                abs(input[0] - input[2]) <= 3 &&
+                abs(input[1] - input[3]) <= 3) {
+                return true
+            } else {
+                println("Enter the cell indexes in format \"letternumberletternumber\" with the ship not expanding in both directions, for example \"a0d0\", \"b3b5\"")
+                return false
+            }
+    }
+}
+
+
+fun gameState(): Boolean? {
     return null
 }
