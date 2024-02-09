64a698a87ff0c762535c0d7c3603fdebb2a83f2e
Goetz Markgraf <goeetz.markgraf@codecentric.de>
2024-02-09 09:29:40 +0100

    with a little help from my friend

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,37 @@
-import kotlin.math.abs
-
 enum class Function {
     CHECK, PLACE
 }
 
+enum class Player {
+    PLAYER1, PLAYER2
+}
 
-data class Ship (val parts: ArrayList<Pair<Int,Int>>)
+data class Position(val col: Int, val row: Int)
 
-val ships: List<Ship> = mutableListOf()
+data class Ship(val parts: ArrayList<Position>)
+
+typealias Field = Array<Array<Int>>
+
+data class GameState(val field1: Field, val field2: Field, val ships1: List<Ship>, val ships2: List<Ship>)
+
 fun main() {
-    val fields = createFields()
-    showField(fields.first)
+    var gameState = createGameState()
+    showField(gameState, Player.PLAYER1)
     while (true) {
         println("Fill")
-        val newOne = place(fields.first)
-        showField(newOne)
+        gameState = place(gameState, Player.PLAYER1)
+        showField(gameState, Player.PLAYER1)
     }
 }
 
-fun createFields(): Pair<Array<Array<Int>>, Array<Array<Int>>> {
+fun createGameState(): GameState {
     val field1 = Array(10) { Array(10) { 0 } }
     val field2 = Array(10) { Array(10) { 0 } }
 
-    return Pair(field1, field2)
+    return GameState(field1, field2, emptyList(), emptyList())
 }
 
-fun showField(field: Array<Array<Int>>) {
+fun showField(gameState: GameState, player: Player) {
     for (i in '0'..'9') {
         print("\t  $i")
     }
@@ -33,13 +39,15 @@ fun showField(field: Array<Array<Int>>) {
 
     val tabVertLine = "\t${9474.toChar()}"
 
-    for (i in 0 until 10) {
-        print(" " + (i + 65).toChar() + tabVertLine)
-        for (j in 0 until 10) {
+    val ships = if (player == Player.PLAYER1) gameState.ships1 else gameState.ships2
+
+    for (row in 0 until 10) {
+        print(" " + (row + 65).toChar() + tabVertLine)
+        for (col in 0 until 10) {
             var shipFound = false
             ships.forEach {
                 it.parts.forEach {
-                    if (it.first == i && it.second == j) {
+                    if (it.row == row && it.col == col) {
                         shipFound = true
                     }
                 }
@@ -53,9 +61,12 @@ fun showField(field: Array<Array<Int>>) {
     }
 }
 
-fun place(field: Array<Array<Int>>): Array<Array<Int>> {
+fun place(gameState: GameState, player: Player): GameState {
     val location = readLine()?.lowercase() ?: ""
     println("inside place")
+
+    val ships = (if (player == Player.PLAYER1) gameState.ships1 else gameState.ships2).toMutableList()
+
     if (inputIsValid(Function.PLACE, location)) {
         val coordinates = convertPair(location)
         //if (coordinates != null) {
@@ -64,27 +75,32 @@ fun place(field: Array<Array<Int>>): Array<Array<Int>> {
 
 
         println(coordinates)
-        val n : ArrayList<Pair<Int,Int>> = ArrayList()
+        val n: ArrayList<Pair<Int, Int>> = ArrayList()
 
-        for (i in firstPos.first .. lastPos.first) {
-            for (j in firstPos.second ..  lastPos.second) {
+        for (i in firstPos.first..lastPos.first) {
+            for (j in firstPos.second..lastPos.second) {
                 println("set at $i,$j")
                 //field[i][j] = 2
-                n.add(Pair(i,j))
+                n.add(Position(i, j))
             }
         }
         ships.addLast(Ship(n))
         //}
     }
-    return field
+    return if (player == Player.PLAYER1) {
+        gameState.copy(ships1 = ships)
+    } else {
+        gameState.copy(ships2 = ships)
+    }
 }
 
 
-fun convertPair(input: String): Pair<Pair<Int,Int>, Pair<Int,Int>> {
+fun convertPair(input: String): Pair<Position, Position> {
     println("in convertPair $input")
-    return Pair(convert(input.substring(0,2)),  convert(input.substring(2,4)))
+    return Pair(convert(input.substring(0, 2)), convert(input.substring(2, 4)))
 }
-fun convert(input: String): Pair<Int, Int>{
+
+fun convert(input: String): Position {
     println("in convert $input")
     val rowChar = input[0]
     val columnChar = input[1]
@@ -95,7 +111,7 @@ fun convert(input: String): Pair<Int, Int>{
     val row = rowChar - 'a'
     val column = columnChar.toString().toInt()
 
-    return Pair(row, column)
+    return Position(row, column)
 }
 
 fun check() {
@@ -120,7 +136,8 @@ fun inputIsValid(function: Function, input: String): Boolean {
             if (input.length == 4 &&
                 input[0] in 'a'..'j' && input[1] in '0'..'9' &&
                 input[2] in 'a'..'j' && input[3] in '0'..'9' &&
-                (input[0] == input[2] || input[1] == input[3])) {
+                (input[0] == input[2] || input[1] == input[3])
+            ) {
                 return true
             } else {
                 println("Enter the cell indexes in format \"letternumber\" for placing ship, for example \"a0\"")
